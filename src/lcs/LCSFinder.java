b7f4514fab73d24afdf04ee548cca3e5db9ac8d1
rovianadz <46758089+rovianadz@users.noreply.github.com>
2023-08-08 20:12:10 -0400

    Update LCSFinder.java

--- src/lcs/LCSFinder.java
+++ src/lcs/LCSFinder.java
@@ -1,51 +1,93 @@
 package lcs;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
-// Node class for the Trie
-class TrieNode {
-    TrieNode[] children;
-    boolean isEndOfWord;
-
-    TrieNode() {
-        children = new TrieNode[26]; // Each node has an array of 26 nodes each representing a letter of the alphabet
-        isEndOfWord = false; // To indicate the end of a word in the Trie
-    }
-}
-
-// Trie class to hold and manipulate the Trie
-class Trie {
-    TrieNode root;
-
-    
-    Trie() {
-        root = new TrieNode();
-    }
-
-    // Method to insert a word into Trie
-    void insert(String word) {
-        TrieNode node = root;
-        for (char ch : word.toLowerCase().toCharArray()) {
-            int index = ch - 'a';
-            if (node.children[index] == null) {
-                node.children[index] = new TrieNode();
-            }
-            node = node.children[index];
-        }
-        node.isEndOfWord = true;
-    }
-}
 
 public class LCSFinder {
-    static Trie trie = new Trie();
-    static List<String> matchingWords = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		readInputFile();	
+	}
+	
+	static void readInputFile() {
+		String filePath = "input.txt";
+		try {
+			Pair<ArrayList<String>, String> fileContent = readFile(filePath);
+			ArrayList<String> inputString = fileContent.getFirst();
+			
+			Trie trie = new Trie();
+			
+			for(int i=0;i<inputString.size();i++) {
+				trie.insert(inputString.get(i));
+			}
+			
+			List<String> findItems = trie.search(fileContent.getSecond());
+			System.out.println(findItems.toString());
+			
+            // Compute LCS of the matching words
+            String ans = "";
+            if(findItems.size() >0) {
+            if (findItems.size() == 1) {
+            	ans=findItems.get(0);
+            	}
+            else if(findItems.size() == 2) {
+            	ans = findLCS(findItems.get(0), findItems.get(1));
+            }else 
+            {
+            	ans=findLCS3Strings(findItems.get(0), findItems.get(1),findItems.get(2));
+            }
 
+			System.out.println(ans);
+	        
+	    }
+            } catch (IOException e) {
+	        System.err.println("Error reading the file: " + e.getMessage());
+	    }
+	}
+	
+    static String findLCS3Strings(String str1, String str2, String str3) {
+        int m = str1.length();
+        int n = str2.length();
+        int p = str3.length();
+
+        int[][][] dp = new int[m + 1][n + 1][p + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= p; k++) {
+                    if (str1.charAt(i - 1) == str2.charAt(j - 1) && str1.charAt(i - 1) == str3.charAt(k - 1)) {
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    } else {
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                    }
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n, k = p;
+        while (i > 0 && j > 0 && k > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1) && str1.charAt(i - 1) == str3.charAt(k - 1)) {
+                lcs.insert(0, str1.charAt(i - 1));
+                i--;
+                j--;
+                k--;
+            } else if (dp[i - 1][j][k] >= dp[i][j - 1][k] && dp[i - 1][j][k] >= dp[i][j][k - 1]) {
+                i--;
+            } else if (dp[i][j - 1][k] >= dp[i - 1][j][k] && dp[i][j - 1][k] >= dp[i][j][k - 1]) {
+                j--;
+            } else {
+                k--;
+            }
+        }
+
+        return lcs.toString();
+    }
+	
     // Method to find the Longest Common Subsequence between two strings
     public static String findLCS(String x, String y) {
         int m = x.length();
@@ -72,68 +114,179 @@ public class LCSFinder {
         return dp[m][n];
     }
 
-    // DFS to traverse the Trie in alphabetical order
-    public static void dfs(TrieNode node, String word) {
-        if (node.isEndOfWord) {
-            matchingWords.add(word);
-        }
-        if (matchingWords.size() == 3) {
-            return;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (node.children[i] != null) {
-                dfs(node.children[i], word + (char) ('a' + i));
-            }
-        }
-    }
-
-    public static void main(String[] args) {
+	public static Pair<ArrayList<String>, String> readFile(String filePath) throws IOException {
+		ArrayList<String> inputString = new ArrayList<>();
+		String regexStr = "";
+		
+	    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+	        String line;
+	        int wordsCount = 0;
+	        
+	        if ((line = br.readLine()) != null) {
+	        	if(isInteger(line)) {
+	        		wordsCount = Integer.parseInt(line);
+	        	}
+	        }
+	        
+	        while ((line = br.readLine()) != null) {
+	        	if(!isInteger(line)) {
+	        		if(wordsCount > 0) {
+	        			inputString.add(line);
+	        		} else {
+	        			regexStr = line;
+	        		}
+	        		wordsCount--;
+	        	}
+	        }
+	    }
+	    Pair<ArrayList<String>, String> content = new Pair<>(inputString, regexStr);
+	    return content;
+	}
+	
+	public static boolean isInteger(String str) {
         try {
-            // Read input from file
-            List<String> lines = Files.readAllLines(Paths.get("input.txt"));
-            int n = Integer.parseInt(lines.get(0));
-            String[] dictionary = new String[n];
-
-            for (int i = 0; i < n; i++) {
-                dictionary[i] = lines.get(i + 1);
-            }
-
-            String regex = lines.get(n + 1);
-
-            // Compile the regex pattern
-            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-
-            // Insert matching words into Trie
-            for (String word : dictionary) {
-                if (pattern.matcher(word).matches()) {
-                    trie.insert(word);
-                }
-            }
-
-            // Traverse Trie and find the first 3 matching words
-            dfs(trie.root, "");
-
-            // Compute LCS of the matching words
-            String lcsResult = "";
-            if (matchingWords.size() >= 2) {
-                lcsResult = findLCS(matchingWords.get(0), matchingWords.get(1));
-                for (int i = 2; i < matchingWords.size(); i++) {
-                    lcsResult = findLCS(lcsResult, matchingWords.get(i));
-                }
-            }
-
-            // Write LCS to output file
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-                writer.write(lcsResult);
-                System.out.println("result writte to output file");
-                System.out.println("result:");
-                System.out.println(lcsResult);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
+
+class Pair<T, U> {
+	private final T first;
+    private final U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    
+    
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+}
+
+
+class TrieNode {
+    private final TrieNode[] children;
+    private boolean isEndOfWord;
+    private Map<String, Integer> matchedStringFreq;
+
+    public TrieNode() {
+        this.children = new TrieNode[26]; // Assuming only lowercase English letters
+        this.isEndOfWord = false;
+        this.matchedStringFreq = new HashMap<>();
+    }
+
+    public void insert(String word) {
+        TrieNode node = this;
+        word = word.toLowerCase(); // Convert to lowercase
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public List<String> search(String word) {
+        matchedStringFreq.clear();
+        word = word.toLowerCase();
+        searchRegex(word, 0, this, new StringBuilder());
+        List<Map.Entry<String, Integer>> sortedList = sortByFrequency(matchedStringFreq);
+        return getTopNStrings(sortedList, 3);
+    }
+
+    private void searchRegex(String word, int index, TrieNode node, StringBuilder currentMatch) {
+        if (index == word.length()) {
+            if (node.isEndOfWord) {
+                String match = currentMatch.toString();
+                matchedStringFreq.put(match, matchedStringFreq.getOrDefault(match, 0) + 1);
+            }
+            return;
+        }
+
+        char ch = word.charAt(index);
+        if (ch == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (node.children[i] != null) {
+                    currentMatch.append((char) (i + 'a'));
+                    searchRegex(word, index + 1, node.children[i], currentMatch);
+                    currentMatch.deleteCharAt(currentMatch.length() - 1);
+                }
+            }
+        } 
+        else if (ch == '*') {
+            if (index == 0 || word.charAt(index - 1) == '.') {
+                for (int i = 0; i < 26; i++) {
+                    if (node.children[i] != null) {
+                        currentMatch.append((char) (i + 'a'));
+                        searchRegex(word, index + 1, node.children[i], currentMatch);
+                        currentMatch.deleteCharAt(currentMatch.length() - 1);
+                    }
+                }
+            } else {
+                char precedingChar = word.charAt(index - 1);
+                int precedingIndex = precedingChar - 'a';
+
+                searchRegex(word, index + 1, node, currentMatch);
+                if (node.children[precedingIndex] != null) {
+                    currentMatch.append(precedingChar);
+                    searchRegex(word, index, node.children[precedingIndex], currentMatch);
+                    currentMatch.deleteCharAt(currentMatch.length() - 1);
+                }
+            }
+        }
+            
+         else {
+            int childIndex = ch - 'a';
+            if (node.children[childIndex] != null) {
+                currentMatch.append(ch);
+                searchRegex(word, index + 1, node.children[childIndex], currentMatch);
+                currentMatch.deleteCharAt(currentMatch.length() - 1);
+            }
+        }
+    }
+
+    private List<Map.Entry<String, Integer>> sortByFrequency(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        return list;
+    }
+
+    private List<String> getTopNStrings(List<Map.Entry<String, Integer>> sortedList, int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < n && i < sortedList.size(); i++) {
+            result.add(sortedList.get(i).getKey());
+        }
+        return result;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        root.insert(word);
+    }
+
+    public List<String> search(String word) {
+        return root.search(word);
+    }
+}
+
+
 
 
